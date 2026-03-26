d14c00b28c84afa6083f43fd4da721b205fd7d76
Axel Asp <solidtop.asp@gmail.com>
2026-03-26 17:21:06 +0100

    First commit

---
+++ LunarStorm.Server/Program.cs
@@ -0,0 +1,17 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.Run();
