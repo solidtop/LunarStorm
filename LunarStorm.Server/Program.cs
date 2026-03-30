6f982ce7806be1a2f8b8b0bfdc26cc96a8957861
Axel Asp <solidtop.asp@gmail.com>
2026-03-30 16:48:12 +0200

    Remove comments & enviroment check

--- LunarStorm.Server/Program.cs
+++ LunarStorm.Server/Program.cs
@@ -2,16 +2,11 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference();
-}
+app.MapOpenApi();
+app.MapScalarApiReference();
 
 app.Run();
